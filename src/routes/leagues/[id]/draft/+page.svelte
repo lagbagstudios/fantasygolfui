b722ad0902dc6c0d7dd018fef53b594cdfa8484e
Anders Olson <sredna43@gmail.com>
2024-03-24 16:53:07 -0500

    draft board improvements

--- src/routes/leagues/[id]/draft/+page.svelte
+++ src/routes/leagues/[id]/draft/+page.svelte
@@ -28,16 +28,47 @@
 	$: leagueId = $page.params.id;
 </script>
 
-<div class="container p-8 sm:flex">
-	<h1 class="h1 pb-8 sm:pb-0 text-center sm:text-left sm:inline-flex sm:w-full">Draft Board</h1>
+<div class="container p-8">
 	<a href="/leagues/{leagueId}" class="btn variant-ghost w-full sm:w-auto sm:float-right"
 		>Return to League Home</a
 	>
+	<h1 class="h1 pt-8 sm:pt-0 sm:ml-4 text-center sm:w-full">Draft Board</h1>
 </div>
 
-<h4 class="h4 text-center pb-8">Budget Remaining: ${remainingBudget}</h4>
+<div class="sm:flex mx-auto space-x-8 w-full p-4">
+	<h2 class="h2 text-center sm:text-left">Your Team</h2>
+</div>
 
-<div class="overflow-scroll w-full">
+<div class="table-container space-y-4 pb-8 sm:w-2/3 max-h-52">
+	<table class="table">
+		<thead>
+			<tr>
+				<th>Golfer</th>
+				<th>Price</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each selectedGolfers as golfer}
+				<tr on:click={() => onGolferSelected(golfer?.golfer_id)}>
+					<td>{golfer?.first_name} {golfer?.last_name}</td>
+					<td>${golfer?.price}</td>
+				</tr>
+			{/each}
+		</tbody>
+		<tfoot>
+			<tr>
+				<th>Budget Remaining</th>
+				<td>${remainingBudget}</td>
+			</tr>
+		</tfoot>
+	</table>
+</div>
+
+<div class="sm:flex mx-auto space-x-8 w-full p-4">
+	<h2 class="h2 text-center sm:text-left">Tournament Field</h2>
+</div>
+
+<div class="overflow-scroll w-full max-h-96">
 	<div class="table-container">
 		<table class="table table-interactive">
 			<thead>
@@ -55,15 +86,10 @@
 					>
 						<td>{golfer?.first_name} {golfer?.last_name}</td>
 						<td>{golfer?.owgr || 'Unranked'}</td>
-						<td>{golfer?.price}</td>
+						<td>${golfer?.price}</td>
 					</tr>
 				{/each}
 			</tbody>
 		</table>
 	</div>
-</div>
-
-<div class="container grid grid-cols-2">
-	<button>test</button>
-	<button>reset</button>
 </div>
